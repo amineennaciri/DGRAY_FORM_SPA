8fe7e68ee041265df0b90050dc2d29a5ec648e7c
amineennaciri <spitlight.enn@gmail.com>
2023-12-30 19:39:07 +0100

    update

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState, useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from './api/axios';
+import axios from '../api/axios';
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
@@ -61,17 +61,20 @@ const Register = () => {
         }
         try {
             const response = await axios.post(REGISTER_URL,
-                JSON.stringify({user, pwd}),
+                JSON.stringify({ user, pwd }),
                 {
-                    headers: {'Content-type':'application/json'},
+                    headers: { 'Content-Type': 'application/json' },
                     withCredentials: true
                 }
             );
-            console.log(response.data);
+            /* console.log(response.data);
             console.log(response.accessToken);
-            console.log(JSON.stringify(response));
+            console.log(JSON.stringify(response)); */
             setSuccess(true);
             /* clear the input fields */
+            setUser('');
+            setPwd('');
+            setMatchPwd('');
         } catch (error) {
             if(!error?.response) {
                 setErrMsg('No server response'); /*in case we loose internet connection*/
